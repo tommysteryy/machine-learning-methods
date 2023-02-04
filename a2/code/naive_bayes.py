9e3bdf468bd601b0e802bad1fa149a2d6f4cc01b
Tommy Xu <tommysteryy@gmail.com>
2023-02-04 14:16:57 -0800

    completed first attempt at naive bayes

--- a2/code/naive_bayes.py
+++ a2/code/naive_bayes.py
@@ -38,13 +38,40 @@ class NaiveBayes:
     def fit(self, X, y):
         n, d = X.shape
 
-        raise NotImplementedError()
+        # print(f"X looks like {X}")
+
+        self.n0 = np.sum(y == 0)
+        self.n1 = np.sum(y == 1)
+
+        # print(f"Calculated n1 and n0 to be {self.n1} and {self.n0}")
+
+        ## p(y) or theta_y 
+        self.theta_y1 = self.n1 / n
+        self.theta_y0 = self.n0 / n 
+        # print(f"theta_y is {self.theta_y}")
+
+        ## theta_x_1 is a vector of d x 1, where theta_x_j is the P(x_j = 1 | y = 1)
+        X_ones = X[y == 1]
+        X_zeros = X[y == 0]
+
+        self.theta_x_1 = (np.sum(X_ones, axis = 0)+ self.laplace_smooth )/ (self.n1 + self.laplace_smooth*2)
+        self.theta_x_0 = (np.sum(X_zeros, axis = 0) + self.laplace_smooth) / (self.n0 + self.laplace_smooth*2)
+        # print(f"theta_x looks like {self.theta_x}")
 
 
     def predict(self, X):
-        raise NotImplementedError()
+        n, d = X.shape
 
+        preds = -1*np.ones(n)
 
+        for i in range(n):
+            x_i = X[i]
+            p_x_y1 = self.theta_y1 * np.inner(x_i, self.theta_x_1)
+            p_x_y0 = self.theta_y0 * np.inner(x_i, self.theta_x_0)
+
+            preds[i] = p_x_y1 > p_x_y0
+
+        return preds
 
 
 
