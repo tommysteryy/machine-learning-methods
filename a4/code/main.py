150a8140cd70f83c59f7c047501235df911b5b71
Tommy Xu <tommysteryy@gmail.com>
2023-04-13 20:58:58 -0700

    implemented rejection sampling for estimating conditional probabilities

--- a4/code/main.py
+++ a4/code/main.py
@@ -165,21 +165,49 @@ def mc_gradschool_50():
 
 
 
-@handle("mc-conditional-mc")
+@handle("mc-conditional-exact")
 def mc_conditional_mc():
     p1, pt = load_dataset("gradChain", "p1", "pt")
     model = MarkovChain(p1, pt)
+    print(pt)
+    cond_probs = []
+    total = 0
+    for i in range(p1.size):
+        prob = model.mc_conditionals(3, i, 6, 5)
+        print(f"p(x_3 = {i}, x_6 = 5) = {prob}")
 
-    print(model.mc_conditionals(3, 6, 0))
+        total += prob
+        cond_probs.append(prob)
+    
+    print(cond_probs * 1/total)
+
+    
 
 
 
-@handle("mc-conditional-exact")
+@handle("mc-conditional-mc")
 def mc_conditional_exact():
     p1, pt = load_dataset("gradChain", "p1", "pt")
     model = MarkovChain(p1, pt)
 
-    raise NotImplementedError()
+    n_samples = 10000
+    time = 6
+    samples = model.sample(n_samples, time)
+
+    accepted = 0
+    counts_c = np.zeros(7)
+
+    for sample in samples:
+        x_6 = sample[5]
+        x_3 = sample[2]
+
+        if (x_6 == 5):
+            accepted += 1
+            counts_c[x_3] = counts_c[x_3] + 1
+
+    rejection_rate = 1 - accepted / n_samples
+    print(f"rejection rate = {rejection_rate}")
+    print(f"Estimated probabilities = {counts_c / accepted}")
 
 
 
